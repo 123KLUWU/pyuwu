f0a9dac0d26f9803c256fff954d09d41cab1849e
123KLUWU <139360858+123KLUWU@users.noreply.github.com>
2024-07-25 18:22:58 -0600

    Add files via upload

--- pruebas.py
+++ pruebas.py
@@ -1,5 +1,7 @@
+import tkinter as tk
 from tkinter import * #la libreria para la interfaz grafica
-root = Tk()
+from tkinter import ttk
+root = tk.Tk()
 #esta es la clase sala
 class sala:
     #aqui definimos los atributos de la sala
@@ -10,6 +12,14 @@ class sala:
 
     def __str__(self): #qui el mensaje que nos dara cuando se mande a imprimir la clase de sala
         return f"Sala (El ID de la sala es: {self.idSala}, Esta sala tiene una capacidad de: {self.capacidad} personas y esta {self.disponibilidad})"
+    
+    def set_sala(self, idSala, capacidad, disponibilidad):
+        if isinstance(idSala, capacidad, disponibilidad, str):
+            self._idSala = idSala
+            self._capacidad = capacidad
+            self._disponibilidad = disponibilidad
+        else:
+            raise ValueError("error ")
 
 class reserva: #esta es la clase reserva
     def __init__(self, idReserva, personaQueReservo, horaReservacion): #otra vez se definen los atributos de la clase 
@@ -17,13 +27,38 @@ class reserva: #esta es la clase reserva
         self.personaQueReservo = personaQueReservo #el nombre de la persona que reservo
         self.horaReservacion = horaReservacion # la hora de la reserva
 
+    def set_idReserva(self, idReserva):
+        if isinstance(idReserva, str):
+            self._idReserva = idReserva
+        else:
+            raise ValueError("error ")
+            
+    def set_(self, personaQueReservo):
+        if isinstance(personaQueReservo, str):
+            self._personaQueReservo = personaQueReservo
+        else:
+            raise ValueError("error ")
+            
+    def set_(self, horaReservacion):
+        if isinstance(horaReservacion, str):
+            self._horaReservacion = horaReservacion
+        else:
+            raise ValueError("error ")
+    
     def __str__(self): #el mensaje que nos dara cuando se mande a llamar 
         return f"reserva (hay una reserva con id {self.idReserva}, para la persona {self.personaQueReservo} a la hora {self.horaReservacion})"
         #soy un comentario inutil uwu
         #estas dos variables solo son de prueba para llenar las clases de arriba
+
 sala1 = sala(idSala=1, capacidad=40, disponibilidad="reservada")
 reserva1 = reserva(idReserva=1, personaQueReservo="heisenberg", horaReservacion="3 pm")
 #importante, esta funcion sera la que nos desplegara el texto al usar el boton para mostrar el estado de la sala
+def hacerReservacion():
+entry = ttk.Entry().pack()
+reserva.set_idReserva())
+        
+Button(root, text="hacer reservacion", command=hacerReservacion).pack() #boton para mostrar la informacion de la reserva
+
 def mostrarSala():
         Label(root, text=sala1).pack()
 #el boton para imprimir el estado de la sala
